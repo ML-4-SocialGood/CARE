4e9aaaec7492e2b9e52b86d7a5430a60cd1c346c
VirtueZhao <virtuedizhao@gmail.com>
2024-11-04 22:57:04 +1300

    update

--- start_care_windows.bat
+++ start_care_windows.bat
@@ -7,4 +7,6 @@ start cmd.exe /C "echo BACKEND Terminal && cd backend && npm run start"
 start cmd.exe /C "echo AI SERVER Terminal && conda activate CARE && cd backend\ai_server && python app.py"
 
 timeout /t 2
-start msedge --inprivate http://localhost:5173
+start chrome --incognito http://localhost:5173
+
+%start msedge --inprivate http://localhost:5173

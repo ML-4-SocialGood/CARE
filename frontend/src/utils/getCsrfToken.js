363472f44b58ae2b42b190bf6d289bf6bd0bcea7
Chris Pearce <chris@pearce.org.nz>
2025-06-28 12:39:17 +1200

    Remove unused imports as identified by 'unimported' npm package.

--- frontend/src/utils/getCsrfToken.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-export function getCsrfToken() {
-  const cookieValue = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith("XSRF-TOKEN="))
-    .split("=")[1];
-  return cookieValue;
-}

245100529b09f0c8928b19a3e493b34997640606
Chris Pearce <chris@pearce.org.nz>
2025-06-29 09:29:10 +1200

    Remove signin/out/up profile and users pages & routes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,38 +2,21 @@
 
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/home/Home.jsx";
-import SignUp from "./pages/Authentication/signup/signup.jsx";
 import About from "./pages/about/About.jsx";
 import Faq from "./pages/faq/Faq.jsx";
 import Upload from "./pages/upload/Upload.jsx";
-import Profile from "./pages/profile/Profile.jsx";
-import ProfileUser from "./pages/profile/ProfileUser.jsx";
 import SitePage from "./components/SitePage.jsx";
 import Images from "./pages/images/ViewImages.jsx";
 import Uploads from "./pages/uploads/ViewUploads.jsx";
-import Users from "./pages/users/Users.jsx";
 import UserGuide from "./pages/user-guide/UserGuide.jsx";
 import Error from "./components/Error.jsx";
 import REIDImageGallery from "./pages/reid/ViewREID.jsx";
-import SignIn from "./pages/Authentication/signin/signin.jsx";
 import Result from "./pages/result/Result.jsx";
 
 const router = createHashRouter([
   {
     path: "/",
     element: <SitePage component={<Home />} withHeader wrapperClass="home" />,
-  },
-  {
-    path: "signin",
-    element: (
-      <SitePage component={<SignIn />} withHeader wrapperClass="signin" />
-    ),
-  },
-  {
-    path: "signup",
-    element: (
-      <SitePage component={<SignUp />} withHeader wrapperClass="signin" />
-    ),
   },
   {
     path: "upload",
@@ -45,18 +28,6 @@ const router = createHashRouter([
     path: "images",
     element: (
       <SitePage component={<Images />} withHeader wrapperClass="images" />
-    ),
-  },
-  {
-    path: "/profile",
-    element: (
-      <SitePage component={<Profile />} withHeader wrapperClass="profile" />
-    ),
-  },
-  {
-    path: "/profile/user",
-    element: (
-      <SitePage component={<ProfileUser />} withHeader wrapperClass="profile" />
     ),
   },
   {
@@ -102,10 +73,6 @@ const router = createHashRouter([
     element: (
       <SitePage component={<Result />} withHeader wrapperClass="result-page" />
     ),
-  },
-  {
-    path: "users",
-    element: <SitePage component={<Users />} withHeader wrapperClass="users" />,
   },
   {
     path: "*",
